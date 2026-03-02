03e85740c68ce889d002a65d62e86b2a6efe6ceb
pikapika <zhmzhm7@sjtu.edu.cn>
2026-03-02 13:23:12 +0800

    Fix:fix the bug of notion.ts

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,19 +1,18 @@
-import { Client } from "@notionhq/client"
+// lib/notion.ts
+import { Client } from "@notionhq/client";
 
-const notion = new Client({
-  auth: process.env.NOTION_TOKEN
-})
+export const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
+// 如果类型报错，用 any
 export async function getPosts() {
-  const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID!,
+  const databaseId = process.env.NOTION_DATABASE_ID!;
+  const response = await (notion.databases as any).query({
+    database_id: databaseId,
     filter: {
       property: "Published",
-      checkbox: {
-        equals: true
-      }
-    }
-  })
-
-  return response.results
+      checkbox: { equals: true },
+    },
+    sorts: [{ property: "Created", direction: "descending" }],
+  });
+  return response.results;
 }
